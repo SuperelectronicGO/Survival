ac5904ba5af222762cc1f222db22244322057832
Me <85188801+SuperelectronicGO@users.noreply.github.com>
2022-11-15 21:46:13 -0500

    Deleted old files

--- SurGame/Assets/Scripts/Misc/PrefabStorage.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PrefabStorage : MonoBehaviour
-{
-    public GameObject bareTreeMold;
-    public AreaData data;
-}
